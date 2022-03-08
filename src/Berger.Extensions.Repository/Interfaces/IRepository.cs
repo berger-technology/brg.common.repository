805cba23e59d2a841cdc8b1e7b240f091def27ec
Rafael Cronemberger <rafael@berger.com.br>
2022-03-08 23:19:54 +0400

    Removing external library responsible for the bulk insert operations

--- src/Berger.Extensions.Repository/Interfaces/IRepository.cs
+++ src/Berger.Extensions.Repository/Interfaces/IRepository.cs
@@ -13,11 +13,9 @@ namespace Berger.Extensions.Repository.Interfaces
         T GetByID(Guid id);
         T Add(T element, bool detach = false);
         void Add(IQueryable<T> elements, bool detach = false);
-        void BulkInsert(IQueryable<T> elements);
         void Update(T element);
-        void BulkUpdate(IQueryable<T> elements);
         void Delete(Guid id);
-        void BulkDelete(IQueryable<T> elements);
+        void Delete(IQueryable<T> elements);
         Task<T> AddAsync(T element);
         Task UpdateAsync(T element);
         Task DeleteAsync(Guid id);
